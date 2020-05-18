473b97b86e491284a3b44d5f9415e1f654169550
sumitk68 <imsumitk1@gmail.com>
2020-05-18 16:39:43 -0400

    Login design created

---
+++ app/src/main/java/com/example/ecommerce/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.ecommerce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+    private Button joinNowBtn, loginBtn;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        joinNowBtn = (Button) findViewById(R.id.main_join_btn);
+        loginBtn = (Button) findViewById(R.id.main_login_btn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+}
